37f95763308fe064d4ef4daec897a15865d8d016
mohamed_hani <mohamed_hani@ubunto-20-04.myguest.virtualbox.org>
2024-08-25 21:24:15 +0300

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mohamed_hani/catkin_ws/src
+# Build directory: /home/mohamed_hani/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlesim_project")
---
+++ build/turtlesim_project/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mohamed_hani/catkin_ws/src/turtlesim_project
+# Build directory: /home/mohamed_hani/catkin_ws/build/turtlesim_project
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
