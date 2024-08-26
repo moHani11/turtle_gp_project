4d3a94ac308f0a2b986160d5fb5c2aba08a73df5
moHani11 <mohamedhani004@gmail.com>
2024-08-27 01:22:11 +0300

    spawning and controlling turtles

--- src/turtlesim_project/scripts/spawn.py
+++ src/turtlesim_project/scripts/spawn.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
-import rospy, roslaunch
+import rospy
 from turtlesim.srv import Spawn
 import random, os
+import subprocess
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 os.chdir(current_directory)
 
-launch_file_path = f"../launch/try1.launch"
+launch_file_path = os.path.join(current_directory, "../launch/try1.launch")
     # da launch file el bey3mel run lel node el hatmove el turtle 
     # 34an at7akem fel turtle 3alatool awel mate3ml spawn
 
@@ -18,8 +19,8 @@ launch_file_path = f"../launch/try1.launch"
 def spawn_turtle():
     x = random.uniform(0, 11)
     y = random.uniform(0, 11)
-    angle = random.uniform(0, 360)
-    rospy.init_node('turtle_spawner', anonymous=True)
+    angle = random.uniform(0, 360)  
+    rospy.init_node('turtle_spawner', anonymous=True)  # bey3mel node  betspawn turtle fe makan random
 
     rospy.wait_for_service('/spawn')
 
@@ -27,24 +28,15 @@ def spawn_turtle():
         spawn_turtle = rospy.ServiceProxy('/spawn', Spawn)
         response = spawn_turtle(x, y, angle, '')
         
-        rospy.loginfo(f"Spawned a turtle named {response.name}")
+        rospy.loginfo(f"Spawned a turtle named: {response.name}")
 
     except rospy.ServiceException as e:
         rospy.logerr(f"Service call failed: {e}")
 
+    command = f"roslaunch turtlesim_project moveLaunch.launch turtle_name:={response.name}"
+    # beysta5dem el bash command line 34an ycall el .launch file we yedeelo arguments
 
-    uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-    roslaunch.configure_logging(uuid)
-    launch = roslaunch.parent.ROSLaunchParent(uuid, [launch_file_path])
-
-    launch.start()
-
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutting down.")
-    finally:
-        launch.shutdown()
+    subprocess.run(command, shell=True, capture_output=True, text=True)
 
 if __name__ == '__main__':
     spawn_turtle()
